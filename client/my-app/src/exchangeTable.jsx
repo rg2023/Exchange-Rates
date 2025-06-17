5e16b4706cc60ebf2aaba61c5eb35a6d894a5aea
רחל גרשון | Rachel Gershon <213060965@gov.il>
2025-06-17 17:55:48 +0300

    adding terraform files

--- client/my-app/src/exchangeTable.jsx
+++ client/my-app/src/exchangeTable.jsx
@@ -10,14 +10,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function ExchangeRateTable({ baseCurrency }) {
-  // const backend_url = window.VITE_BACKEND_URL || "";
+  const backend_url = window.VITE_BACKEND_URL || "";
   const [exchangeRates, setExchangeRates] = useState([]);
   const [sorting, setSorting] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch(`__BACKEND_URL__/exchange-rates/${baseCurrency}`);
+        const response = await fetch(`${backend_url}/exchange-rates/${baseCurrency}`);
         if (!response.ok) {
           throw new Error('Failed to fetch exchange rates');
         }
